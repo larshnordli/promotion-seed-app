55947ace350ad54ec19491c92bf374e6823d93ac
Benny Hoang <benny.hoang@accenture.com>
2017-07-13 14:59:04 +0200

    configured webpack and React is good to go!

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 /*ReactDOM.render() here*/
+const React = require('react');
+const ReactDOM = require('react-dom');
+import App from './components/App';
+
+ReactDOM.render(
+  <App/>, document.getElementById('app'));
