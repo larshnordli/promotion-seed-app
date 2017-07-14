bb047d21767d96a1fcf17c36574f3f6fc708ec89
Benny Hoang <benny.hoang@accenture.com>
2017-07-14 14:06:11 +0200

    added section data to own file

--- src/components/App.js
+++ src/components/App.js
@@ -2,29 +2,20 @@ import React from 'react';
 
 import Header from './header/Container';
 import Team from './team/Container';
+import sections from '*/content/content-and-order';
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      section: [
-        {
-          type: "header",
-          headline: "Nandini bjeff woff",
-          text: " <strong>ACIT Cloud Studio's</strong> Prototype for Leonard Chesire Disability"
-        }, {
-          type: "introSection",
-          headline: "Nandini mjeauuuuf",
-          text: " <strong>ACIT Cloud Studio's</strong> Prototype for Leonard Chesire Disability extended"
-        }
-      ]
+      sections: sections
     }
   }
   render() {
     return (
       <div className="container-fluid">
         <div className="row">
-          {this.state.section.map((s,i) => {
+          {this.state.sections.map((s,i) => {
             switch (s.type) {
               case "header":
                 return <Header key={i} headline={s.headline} text={s.text}/>
---
+++ src/content/content-and-order.js
@@ -0,0 +1,14 @@
+var sections = [
+
+  {
+   type: "introSection",
+   headline: "Nandini mjeauuuuf",
+   text: " <strong>ACIT Cloud Studio's</strong> Prototype for Leonard Chesire Disability extended"
+ },{
+    type: "header",
+    headline: "Nandini bjeff woff",
+    text: " <strong>ACIT Cloud Studio's</strong> Prototype for Leonard Chesire Disability"
+  }
+]
+
+export default sections;
