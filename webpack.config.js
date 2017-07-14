0c3758b10e0921f7e64c495bcabde7145c012843
Lars Henrik Nordli <larshenrik91@gmail.com>
2017-07-14 08:26:39 +0200

    prelim attempt scss config in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,50 @@
-var HTMLWebpackPlugin = require ('html-webpack-plugin');
-var HTMLWebpackPluginConfig = new HTMLWebpackPlugin({
-  template: __dirname + '/index.html',
-  filename: 'index.html',
-  inject: 'body'
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const HTMLWebpackPluginConfig = new HTMLWebpackPlugin({
+    template: __dirname + '/index.html',
+    filename: 'index.html',
+    inject: 'body'
 });
+
 module.exports = {
-  entry: __dirname + '/src/index.js',
-  module: {
-    loaders: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader'
-      }
+    entry: __dirname + '/src/index.js',
+    devtool: 'source-map',
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            }, {
+                test: /\.scss$/,
+                use: [
+                    {
+                        loader: ExtractTextPlugin.extract('style-loader', // The backup style loader
+                                'css?sourceMap!sass?sourceMap')
+                    }, {
+                        loader: "sass-loader",
+                        options: {
+                            includePaths: [
+                                __dirname + "/src/styles/main",
+                                __dirname + "build/main.css"
+                            ]
+                        }
+                    }
+                ]
+
+            }
+        ]
+    },
+    output: {
+        filename: 'transformed.js',
+        path: __dirname + '/build'
+    },
+    // sassLoader: {
+    //     includePaths: ['src/styles/main']
+    // },
+    plugins: [
+        HTMLWebpackPluginConfig,
+        new ExtractTextPlugin(__dirname + '/build/main.css')
     ]
-  },
-  output: {
-    filename: 'transformed.js',
-    path: __dirname + '/build'
-  },
-  plugins: [HTMLWebpackPluginConfig]
 };
