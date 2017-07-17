65c60e81aa3a93643e46a141fbfe5137bbd189d8
Benny Hoang <benny.hoang@accenture.com>
2017-07-17 11:38:59 +0200

    Moved getImage method from child components to parent.

--- src/components/header/HeaderContainer.js
+++ src/components/header/HeaderContainer.js
@@ -2,16 +2,12 @@ import React from 'react';
 
 
 export default class Header extends React.Component {
-
-getImage(url){
-  return require('../../media/images/'+url);
-}
   render() {
     return (
       <div className="col-md-6">
         <h1>{this.props.headline}</h1>
         <div dangerouslySetInnerHTML={ {__html: this.props.text} }></div>
-        <img className="img-responsive" src={this.getImage(this.props.imageUrl)}></img>
+        <img className="img-responsive" src={this.props.imageUrl}></img>
       </div>
     )
   }
--- src/content/content-and-order.js
+++ src/content/content-and-order.js
@@ -4,7 +4,7 @@ var sections = [
    type: "introSection",
    headline: "Nandini mjeauuuuf",
    text: " <strong>ACIT Cloud Studio's</strong> Prototype for Leonard Chesire Disability extended",
-   imageUrl: "http://r.ddmcdn.com/s_f/o_1/cx_462/cy_245/cw_1349/ch_1349/w_720/APL/uploads/2015/06/caturday-shutterstock_149320799.jpg"
+   imageUrl: "maxresdefault.jpg"
  },{
     type: "header",
     headline: "Nandini bjeff woff",
