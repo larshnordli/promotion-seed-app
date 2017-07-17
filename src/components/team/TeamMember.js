4ed53430a4ef3725b866da902a7ce62566d1aa03
Benny Hoang <benny.hoang@accenture.com>
2017-07-17 14:07:35 +0200

    centered item and added helper class from bootstrap

--- src/components/team/TeamMember.js
+++ src/components/team/TeamMember.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default class TeamMember extends React.Component {
   render() {
     return(
-      <div className="team-member-item col-md-4">
+      <div className="team-member-item col-md-4 text-center">
         <img className="img-responsive" src={this.props.img}></img>
         <p>{this.props.name}</p>
         <p>{this.props.title}</p>
