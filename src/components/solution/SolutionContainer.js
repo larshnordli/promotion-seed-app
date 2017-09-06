7e64d7bc49ddf213d6eb6d3f82d36cebf8bfb898
Hoang <benny.hoang@accenture.com>
2017-09-06 16:04:02 +0200

    added react-slick (image carousel)

--- src/components/solution/SolutionContainer.js
+++ src/components/solution/SolutionContainer.js
@@ -1,19 +1,29 @@
 import React from 'react';
+import Slider from 'react-slick';
 
 export default class SolutionContainer extends React.Component {
   renderSlideImages(url) {
     return require('../../media/images/' + url);
   }
   render() {
+    const settings = {
+      dots: true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      autoplay: true,
+      arrows: false
+    }
     return (
-      <div>
+      <div className="container">
         <h2>{this.props.headline}</h2>
         <p>{this.props.text}</p>
-        <div>
+        <Slider {...settings}>
           {this.props.imgSlide.map((img, i) => {
-            return (<img src={this.renderSlideImages(img)} className='img-responsive'/>)
+            return (<img key={i} src={this.renderSlideImages(img)} className="img-responsive"/>)
           })}
-        </div>
+        </Slider>
       </div>
     )
   }
