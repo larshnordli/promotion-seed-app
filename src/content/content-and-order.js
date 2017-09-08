d88520733abb00e7818d9c2aa1b9ec054d73e522
Hoang <benny.hoang@accenture.com>
2017-09-08 11:07:32 +0200

    seperated content into own files for easier maintainability on the content order

--- src/content/content-and-order.js
+++ src/content/content-and-order.js
@@ -1,203 +1,23 @@
+const introSection = require('./introSection.json');
+const teamSection = require('./team.json');
+const timelineSection = require('./timeline.json');
+const technologiesSection = require('./technologies.json');
+const solutionSection = require('./solution.json');
+const testimonialSection = require('./testimonial.json');
+const mvpSection = require('./mvp.json');
+const featuresSection = require('./features.json');
+const contactSection = require('./contact.json');
+
 var sections = [
-
-  {
-    "type": "introSection",
-    "headline": "Nandini mjeauuuuf",
-    "text": " <strong>ACIT Cloud Studio's</strong> Prototype for Leonard Chesire Disability extended",
-    "imageUrl": "maxresdefault.jpg"
-  }, {
-    type: "team",
-    headline: "The Team",
-    text: " <strong>Team text </strong>",
-    team: [
-      {
-        "name": "Alba",
-        "title": "UI designer",
-        "imageUrl": "alba.fernandez.png.jpg"
-      }, {
-        "name": "Christer",
-        "title": "Prototype Lead",
-        "imageUrl": "alba.fernandez.png.jpg"
-      }, {
-        "name": "Jonny",
-        "title": "Cloud Studio Lead",
-        "imageUrl": "alba.fernandez.png.jpg"
-      }, {
-        "name": "Kjetil",
-        "title": "Prototype Lead",
-        "imageUrl": "alba.fernandez.png.jpg"
-      }, {
-        "name": "Lars Henrik",
-        "title": "Front-end Developer",
-        "imageUrl": "alba.fernandez.png.jpg"
-      }, {
-        "name": "Ruben",
-        "title": "Front-end Developer",
-        "imageUrl": "alba.fernandez.png.jpg"
-      }, {
-        "name": "Siri",
-        "title": "Analytics Specialist",
-        "imageUrl": "alba.fernandez.png.jpg"
-      }, {
-        "name": "Tiril",
-        "title": "UX Designer",
-        "imageUrl": "alba.fernandez.png.jpg"
-      }, {
-        "name": "Umesh",
-        "title": "UX Tester",
-        "imageUrl": "alba.fernandez.png.jpg"
-
-      }
-    ]
-  }, {
-    "type": "timeline",
-    "headline": "timeline",
-    "text": " <strong>ACIT Cloud Studio's</strong> Prototype for Leonard Chesire Disability extended",
-    "imageUrl": "maxresdefault.jpg",
-    "phases": [
-      {
-        "title": "Phase 1: 4 weeks",
-        "phase": [
-          {
-            "title": "<strong>Week 1</strong> Design Thinking Workshop in London",
-            "description": "Initial creative workshop in London, UK, to ideate on concept and define scope. Attendees from LCD UK and India, as well as Accenture Corporate Citizenship and ACIT Cloud Studio"
-          }, {
-            "title": "<strong>Week 1</strong> Low Fidelity Prototyping",
-            "description": "Refined concepts from the initial workshop, creating and iterating paper & digital wireframes."
-          }
-        ]
-      }, {
-        "title": "Phase 2: 4 additional weeks",
-        "phase": [
-          {
-            "title": "<strong>Week 5</strong> Virtual design thinking WS",
-            "description": "Online workshop with key stakeholders to align on scope for the second phase, and determine foucus areas."
-          }, {
-            "title": "<strong>Week 5</strong> Refine Design & Content",
-            "description": "Refined solution based on findings and feedback from the frist phase. New iterataions on design and user experience."
-          }
-        ]
-      }
-    ]
-  }, {
-    "type": "technologies",
-    "headline": "Technologies",
-    "text": "Nandini leverages the IBM Bluemix Platform along with IBM Watson Conversation, Tone Analyzer, Text-to-Speech and Speech-to-Text services.",
-    "imageUrl": null,
-    "tools": [
-      {
-        "imageUrl": 'bluemix.png',
-        "name": "IBM Bluemix",
-        "link": "https://console.ng.bluemix.net/"
-      }, {
-        "imageUrl": 'conversation.png',
-        "name": "Conversation",
-        "link": "https://www.ibm.com/watson/developercloud/conversation.html"
-      }, {
-        "imageUrl": 'service-icon.png',
-        "name": "Tone Analyzer",
-        "link": "https://www.ibm.com/watson/developercloud/tone-analyzer.html"
-      }, {
-        "imageUrl": 'text-to-speech.png',
-        "name": "Text to Speech",
-        "link": "https://www.ibm.com/watson/developercloud/text-to-speech.html"
-      }, {
-        "imageUrl": 'speech-to-text.png',
-        "name": "Speech to Text",
-        "link": "https://www.ibm.com/watson/developercloud/speech-to-text.html"
-      }, {
-        "imageUrl": 'watson.png',
-        "name": "IBM Watson",
-        "link": "https://www.ibm.com/watson/"
-      }
-    ]
-  }, {
-    "type": "solution",
-    "headline": "The Solution",
-    "text": "We worked with Leonard Cheshire Disability (LCD) in an iterative design thinking approach over two 4-week phases to build a virtual registration assistant named Nandini, which supports job seekers with varied disabilities to digitally register themselves in an easy, engaging and accessible manner.",
-    "imageUrl": null,
-    "imgSlide": [
-      "200_s.gif",
-      "Cat.jpg",
-      "maxresdefault.jpg"
-    ]
-  }, {
-    "type": "testimonials",
-    "headline": "Testimonials",
-    "items": [
-      {
-        "backgroundImg": "maxresdefault.jpg",
-        "stakeholderImg": "alba.fernandez.png.jpg",
-        "stakeholderName": "Alessandra Furtado",
-        "stakeholderTitle": "assistant director informational resourcing & developement LCD",
-        "text": "This is description",
-        "logo": "bluemix.png"
-      },
-      {
-        "backgroundImg": "Cat.jpg",
-        "stakeholderImg": "alba.fernandez.png.jpg",
-        "stakeholderName": "Furtado Alessandra ",
-        "stakeholderTitle": "assistant director informational resourcing & developement LCD",
-        "text": "This is description",
-        "logo": "watson.png"
-      }
-    ]
-  }, {
-    "type": "mvp",
-    "headline": "Mvp statement",
-    "text": "with <strong>guidance</strong> from a <strong>virtual assistant</strong>",
-    "backgroundImg": "Cat.jpg",
-  }, {
-    "type": "features",
-    "items": [
-      {
-        "headline": "guided <strong>Virtual Agent<strong/>",
-        "text": "Nandini is a virtual agent that guides you through six sections of questions",
-        "icon": "watson.png"
-      },
-      {
-        "headline": "Adaptive and natural <strong>Dialogue<strong/>",
-        "text": "Throughout the process, nandini provides multiple choice answer",
-        "icon": "bluemix.png"
-      },
-      {
-        "headline": "Low vision <strong>Accesibility<strong/>",
-        "text": "The entire solution is keyboard navigatable and WCAG 2.0 AA compliant",
-        "icon": "conversation.png"
-      },
-      {
-        "headline": "Audio <strong>Interface<strong/>",
-        "text": "Nandini supports speech-to-text",
-        "icon": "speech-to-text.png"
-      }
-
-    ]
-  }, {
-    "type": "contact",
-    "headline": "Contact us",
-    "items": [
-      {
-        "icon": "watson.png",
-        "text": "Send us email",
-        "link": "www.wikipedia.com"
-      },
-      {
-        "icon": "bluemix.png",
-        "text": "Visit our home",
-        "link": "www.wikipedia.com"
-      },
-      {
-        "icon": "speech-to-text.png",
-        "text": "About cloud studio",
-        "link": "www.wikipedia.com"
-      },
-      {
-        "icon": "conversation.png",
-        "text": "Demo catalog",
-        "link": "www.wikipedia.com"
-      }
-    ]
-  }
+  introSection, 
+  mvpSection, 
+  solutionSection,  
+  technologiesSection,  
+  featuresSection,   
+  timelineSection,  
+  testimonialSection,   
+  teamSection,
+  contactSection
 ]
 
 export default sections;
